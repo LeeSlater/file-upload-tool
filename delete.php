52281fec1d0b3ef56a0c87f5b9dd602a84f36cae
Lee Slater <lslater@slateaf1.miniserver.com>
2019-03-29 17:13:52 +0000

    Moved most of the server-side code into class.file_manager.php for easier configuration and because it made me happier

--- delete.php
+++ delete.php
@@ -24,29 +24,15 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * Scan the uploads directory and delete the defined file if found
  */
 
-$uploads_root = "/var/www/site/public_html/custom/file-uploads/uploads";
-$uploads_dir = $uploads_root;
+ini_set('display_errors', true);
+
+require_once("class.file_manager.php");
+$file_manager = new file_manager();
+
 if (isset($_POST['app_id'])) {
-	if (file_exists($uploads_root.'/'.$_POST['app_id'])) {
-		$uploads_dir = $uploads_root.'/'.$_POST['app_id'];
-	}
-}
-if (isset($_POST['file_name'])) {
-	scan_dir_for_file($uploads_dir, $_POST['file_name']);
-}
-
-function scan_dir_for_file($dir, $file) {
-	$files = scandir($dir);
-	foreach ($files as $f) {
-		if ($f=="." || $f=="..") continue;
-		echo " ".$f." ";
-		if (is_dir($dir.'/'.$f)) {
-			scan_dir_for_file($dir.'/'.$f, $file);
-		} else {
-			if ($f==$file) {
-				unlink($dir.'/'.$f);
-			}
-		}
+	if (file_exists($file_manager->uploads_root.'/'.$_POST['app_id'])) {
+		$file_manager->uploads_dir = $file_manager->uploads_root.'/'.$_POST['app_id'];
 	}
 }
 
+$file_manager->delete_file($_POST['file_name']);
