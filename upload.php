c1fbaf278367ed3d01dedc90ed0429055f3ac9a5
Lee Slater <lslater@slateaf1.miniserver.com>
2019-04-03 11:45:06 +0000

    Added session_start() to allow for userFormID to be worked out

--- upload.php
+++ upload.php
@@ -20,6 +20,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 
+session_start();
+
 require_once("class.file_manager.php");
 $file_manager = new file_manager();
 
